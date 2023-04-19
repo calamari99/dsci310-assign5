891880ea46aad3bea2ed4d196647126873644b89
Enoch Cheung <ajienoch@gmail.com>
2023-04-18 18:17:40 -0700

    Initial creation

---
+++ toyscript.R
@@ -0,0 +1,11 @@
+#how to create a new package
+#1. call dev tools
+library(devtools)
+# devtools::load_all()
+# 
+packageVersion("devtools")
+#2. choose path to create package
+create_package("/path/regexcite")
+
+#3. in new Rstudio run devtools again and open regexcite.Rproj
+
